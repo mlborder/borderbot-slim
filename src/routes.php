1983ba4f1f3ea17f73e9fe6d8f88eea606e62c48
treby <treby@atelier-nodoka.net>
2016-01-06 21:17:51 +0900

    Simple REST API logic.

--- src/routes.php
+++ src/routes.php
@@ -1,1 +1,67 @@
 <?php
+// Find by event_id
+$app->get('/events/{id}/records', function($request, $response, $args) {
+    $event_id = intval($args['id']);
+    $params = $request->getQueryParams();
+
+    if (isset($params['idol_id'])) {
+        $idol_id = intval($params['idol_id']);
+        $results = ORM::for_table('event_results')
+            ->where('event_id', $event_id)
+            ->where('idol_id', $idol_id)
+            ->order_by_asc('rank')
+            ->limit(5000)
+            ->find_many();
+    } else {
+        $results = ORM::for_table('event_results')
+            ->where('event_id', $event_id)
+            ->order_by_asc('idol_id')
+            ->order_by_asc('rank')
+            ->limit(5000)
+            ->find_many();
+    }
+
+    $rankings = [];
+    foreach ($results as $result) {
+        array_push($rankings, [
+            'id' => $result->id,
+            'event_id' => $result->event_id,
+            'idol_id' => $result->idol_id,
+            'rank' => $result->rank,
+            'point' => $result->point,
+            'player_id' => $result->player_id
+        ]);
+    }
+
+    $response->write(json_encode($rankings));
+    return $response->withHeader('Content-type', 'application/json');
+});
+
+// Find player by id
+$app->get('/players/{id}', function ($request, $response, $args) use ($app){
+    $ret = ['id' => $args['id']];
+    $response->write(json_encode($ret));
+    return $response->withHeader('Content-type', 'application/json');
+});
+
+// Find by player_id
+$app->get('/players/{id}/records', function ($request, $response, $args) use ($app){
+    $results = ORM::for_table('event_results')
+        ->where('player_id', $args['id'])
+        ->order_by_asc('event_id')
+        ->find_many();
+
+    $ret = [];
+    foreach ($results as $result) {
+        array_push($ret, [
+            'id' => $result->id,
+            'event_id' => $result->event_id,
+            'idol_id' => $result->idol_id,
+            'rank' => $result->rank,
+            'point' => $result->point,
+            'player_id' => $result->player_id
+        ]);
+    }
+    $response->write(json_encode($ret));
+    return $response->withHeader('Content-type', 'application/json');
+});
