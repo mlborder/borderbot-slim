66a63c5d0dd15b215c2772063163b45aa7515a0f
treby <treby@atelier-nodoka.net>
2017-02-14 01:38:22 +0900

    reveal name column for player_record table.

--- src/models/week_record.php
+++ src/models/week_record.php
@@ -41,32 +41,32 @@ class WeekRecord
 
         if ($idol_id) {
             $results = ORM::for_table(self::SLIM_DB_TABLE)
-                ->select_many_expr(['week_id', 'player_id', 'fan_count' => 'SUM(fan_count)'])
+                ->select_many_expr(['week_id', 'player_id', 'name', 'fan_count' => 'SUM(fan_count)'])
                 ->where('week_id', intval($week_id))
                 ->where('idol_id', intval($idol_id))
                 ->group_by('player_id')
-                ->left_outer_join('players', ['player_id', '=', 'players.id'])
                 ->offset($offset)->limit($limit)
+                ->left_outer_join('players', [self::SLIM_DB_TABLE . '.player_id', '=', 'players.id'])
                 ->order_by_desc('fan_count')->order_by_asc('player_id')->find_many();
         } else {
             $results = ORM::for_table(self::SLIM_DB_TABLE)
-                ->select_many_expr(['week_id', 'player_id', 'fan_count' => 'SUM(fan_count)'])
+                ->select_many_expr(['week_id', 'player_id', 'name', 'fan_count' => 'SUM(fan_count)'])
                 ->where('week_id', intval($week_id))
                 ->group_by('player_id')
-                ->left_outer_join('players', ['player_id', '=', 'players.id'])
                 ->offset($offset)->limit($limit)
+                ->left_outer_join('players', [self::SLIM_DB_TABLE . '.player_id', '=', 'players.id'])
                 ->order_by_desc('fan_count')->order_by_asc('player_id')->find_many();
         }
 
         $ret = [];
         foreach ($results as $i => $result) {
             array_push($ret, [
-                'rank' => $i + $offset + 1,
-                'week_id' => $result->week_id,
-                'idol_id' => $idol_id,
+                'rank' => intval($i + $offset + 1),
+                'week_id' => intval($result->week_id),
+                'idol_id' => intval($idol_id),
                 'player_id' => $result->player_id,
                 'name' => $result->name,
-                'fan_count' => $result->fan_count,
+                'fan_count' => intval($result->fan_count),
             ]);
         }
 
